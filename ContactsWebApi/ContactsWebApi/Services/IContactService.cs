d7baf4e3b77143aca5444b7245e4276dd6e68fcc
akitiainen <aki.tiainen@hotmail.fi>
2019-06-03 10:38:44 +0300

    created new project
    Couldn't update .NET core to v2.2 so had to create whole new project

---
+++ ContactsWebApi/ContactsWebApi/Services/IContactService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactsWebApi.Models;
+
+namespace ContactsWebApi.Services
+{
+    public interface IContactService
+    {
+        Contact Create(Contact contact);
+        void Delete(int id);
+        List<Contact> Read();
+        Contact Read(int id);
+        Contact Update(int id, Contact contact);
+    }
+}
