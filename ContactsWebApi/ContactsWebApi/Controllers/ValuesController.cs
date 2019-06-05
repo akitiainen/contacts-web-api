4a1046fbc91e07771d3897d5e3dfad67dae15533
akitiainen <aki.tiainen@hotmail.fi>
2019-06-05 13:49:04 +0300

    ContactsWebApi
    Added my own azure database

--- ContactsWebApi/ContactsWebApi/Controllers/ValuesController.cs
+++ ContactsWebApi/ContactsWebApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace ContactsWebApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "es", "make" };
         }
 
         // GET api/values/5
